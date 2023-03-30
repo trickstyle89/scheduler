ea02ad98943f3cde276542540b7d116fcb0125ae
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-03-30 16:20:37 -0400

    Completed Cypress Testing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,12 @@
 describe("Navigation", () => {
     it("should visit root", () => {
-      cy.visit("/");
+        cy.visit("/");
     });
+    it("should navigate to Tuesday", () => {
+        cy.contains("[data-testid=day]", "Tuesday")
+            .click()
+            .should("have.class", "day-list__item--selected");
+
   });
+});
+
